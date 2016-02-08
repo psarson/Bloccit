5ebc3b89cc1a72c4790b600084e4803dc2e8734b
Patrick Sarson <sarsonmedia@gmail.com>
2016-02-08 13:16:56 -0500

    Add Assignment 28

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true) }
+
+    describe "attributes" do
+
+      it "responds to title" do
+        expect(question).to respond_to(:title)
+      end
+
+      it "responds to body" do
+        expect(question).to respond_to(:body)
+      end
+      it "responds to resolve" do
+        expect(question).to  respond_to(:resolved)
+      end
+    end
+end
