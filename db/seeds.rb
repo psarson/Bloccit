320bec8a29c6b0ffc78dca7f3328ae6b043dcd24
Patrick Sarson <sarsonmedia@gmail.com>
2016-02-08 20:56:21 -0500

    Finished Assignment 30

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,32 @@
 require "random_data"
 
 50.times do
- # #1
+
    Post.create!(
- # #2
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
 
+ uniq_post = Post.find_or_create_by(
+    title: "Awesome Post!",
+    body: "This is the most awesome body of text ever!"
+ )
+
  posts = Post.all
 
- # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
+
+
+ uniq_comments = Comment.find_or_create_by(
+   post: uniq_post,
+   body: "Hey! A body!"
+ )
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
