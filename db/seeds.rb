1a6d0674b5cb67d35bb5c9808d11e18c96b1b8d3
Patrick Sarson <sarsonmedia@gmail.com>
2016-02-08 19:22:07 -0500

    Add Checkpoint 30

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+require "random_data"
+
+50.times do
+ # #1
+   Post.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
