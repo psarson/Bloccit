0f3b0f1a5e95dec9d4c7e10a933cb6e135c55a94
Patrick Sarson <sarsonmedia@gmail.com>
2016-02-21 13:29:38 -0500

    Add Checkpoint 37

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   before_save { self.email = email.downcase }
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -10,13 +11,4 @@ class User < ActiveRecord::Base
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
    has_secure_password
- end
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> checkpoint-36-user_authentication
-=======
-
->>>>>>> checkpoint-36-user_authentication
 end
