c9e1041cb5c0f465e1b686015b24c7079f79633b
Patrick Sarson <sarsonmedia@gmail.com>
2016-02-29 14:40:16 -0500

    Add Assignment 39

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order(title: :asc)}
+  scope :ordered_by_reverse_created_at, -> { order(:created_at).reverse_order}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
