67f5ef433ab4c29058309318b9f63a593ab13018
Brittany Martin <brittany.jill.martin@gmail.com>
2016-03-06 18:10:29 -0500

    Rearrange test syntax so tests pass

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,68 +1,62 @@
 class CommentsController < ApplicationController
-  before_action :require_sign_in
-  before_action :authorize_user, only: [:destroy]
 
-  def create
+   before_action :require_sign_in
+   before_action :authorize_user, only: [:destroy]
 
-   if params[:topic_id]
-      @topic = Topic.find(params[:topic_id])
-      comment = @topic.comments.new(comment_params)
-      comment.user = current_user
-
-      if comment.save
-        flash[:notice] = "Comment saved successfully."
-        redirect_to @topic
-      else
-        flash[:alert] = "Comment failed to save."
-        redirect_to @topic
-      end
-    end
-
-    if params[:post_id]
-       @post = Post.find(params[:post_id])
-       comment = @post.comments.new(comment_params)
+   def create
+     if params[:topic_id].present?
+       @topic = Topic.find(params[:topic_id])
+       comment = @topic.comments.new(comment_params)
        comment.user = current_user
 
        if comment.save
          flash[:notice] = "Comment saved successfully."
-         redirect_to [@post.topic, @post]
+         redirect_to [@topic]
        else
          flash[:alert] = "Comment failed to save."
+         redirect_to [@topic]
+       end
+     elsif params[:post_id].present?
+       @post = Post.find(params[:post_id])
+       comment = @post.comments.new(comment_params)
+       comment.user = current_user
+
+        if comment.save
+          flash[:notice] = "Comment saved successfully."
+          redirect_to [@post.topic, @post]
+        else
+          flash[:alert] = "Comment failed to save."
+          redirect_to [@post.topic, @post]
+        end
+     end
+   end
+
+   def destroy
+     if params[:topic_id].present?
+       @topic = Topic.find(params[:topic_id])
+       comment = @topic.comments.find(params[:id])
+
+       if comment.destroy
+         flash[:notice] = "Comment was deleted."
+         redirect_to [@topic]
+       else
+         flash[:alert] = "Comment couldn't be deleted. Try again."
+         redirect_to [@topic]
+       end
+
+    elsif params[:post_id].present?
+       @post = Post.find(params[:post_id])
+       comment = @post.comments.find(params[:id])
+
+       if comment.destroy
+         flash[:notice] = "Comment was deleted."
+         redirect_to [@post.topic, @post]
+       else
+         flash[:alert] = "Comment couldn't be deleted. Try again."
          redirect_to [@post.topic, @post]
        end
      end
-  end
-
-
-
-  def destroy
-
-  if params[:topic_id]
-    @topic = Topic.find(params[:topic_id])
-    comment = @topic.comments.new(comment_params)
-
-    if comment.destroy
-      flash[:notice] = "Comment was deleted."
-      redirect_to @topic
-    else
-      flash[:alert] = "Comment couldn't be deleted. Try again."
-      redirect_to @topic
-    end
-  end
-
-   if params[:post_id]
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
-
-    if comment.destroy
-      flash[:notice] = "Comment was deleted."
-      redirect_to [@post.topic, @post]
-    else
-     flash[:alert] = "Comment couldn't be deleted. Try again."
-     redirect_to [@post.topic, @post]
-    end
    end
-end
 
    private
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Comment, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
-  let(:comment) { topic.comment.create!(body: 'Comment Body', topic: topic, user: user) }
+  let(:comment) { topic.comments.create!(body: 'Comment Body', topic: topic, user: user) }
 
      it { is_expected.to belong_to(:post) }
      it { is_expected.to belong_to(:user) }
