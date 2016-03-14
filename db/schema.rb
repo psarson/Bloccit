d6eca90243b2b14e5ed1fa6e52ec8e7f2016d868
Patrick Sarson <sarsonmedia@gmail.com>
2016-03-14 13:54:50 -0400

    Add Checkpoint-48

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308005010) do
+ActiveRecord::Schema.define(version: 20160314042307) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160308005010) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -113,4 +113,9 @@ end
      end
    end
 
+   describe "#generate_auth_token" do
+     it "creates a token" do
+       expect(user.auth_token).to_not be_nil
+     end
+   end
 end
